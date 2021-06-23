d6f010f02ceb6b8f96ec947a657dfb5ef332f3f2
Jon Hagg <jhagg314@gmail.com>
2021-06-23 00:27:56 -0700

    writing some code

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import praw
 import json
 from pathlib import Path
+from tqdm import tqdm
 
 
 class SaveReddit:
     def __init__(self) -> None:
-        self._client = self.get_client()
+        self._client = None
         self._saved = None
 
-    def get_client(self):
-        creds = Path(Path(__file__).parent.absolute(), "auth.json")
-        with open(creds) as f:
-            auth = json.load(f)
-        return praw.Reddit(**auth)
+    @property
+    def client(self):
+        if self._client is None:
+            creds = Path(Path(__file__).parent.absolute(), "auth.json")
+            with open(creds) as f:
+                auth = json.load(f)
+            self._client = praw.Reddit(**auth)
+        return self._client
 
-    def get_saved(self):
+    @property
+    def saved(self):
         if self._saved is None:
-            self._saved = [s for s in self._client.user.me().saved()]
+            self._saved = [s for s in self.client.user.me().saved()]
         return self._saved
+
+    def sort_saved(self):
+        return sorted(self.saved, key=lambda x: str(type(x)))
+
+    def extract(self, name):
+        result = []
+        for s in tqdm(self.saved):
+            try:
+                result.append(getattr(s, name))
+            except:
+                pass
+        return result
+
+    def extract_comments(self):
+        return self.extract("body")
+
+    def extract_title(self):
+        return self.extract("title")
